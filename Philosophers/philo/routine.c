89dc2bccd65598486431522d2509430cad72e949
jasongoo827 <81475273+jasongoo827@users.noreply.github.com>
2024-04-03 13:42:21 +0900

    Update routine.c

--- Philosophers/philo/routine.c
+++ Philosophers/philo/routine.c
@@ -2,42 +2,41 @@
 
 void	*monitor(void *ptr)
 {
-	t_philo	*philo;
+	t_data	*data;
 
-	philo  = (t_philo *)ptr;
-	// pthread_mutex_lock(&philo->data->read);
-	while (philo->data->dead == 0)
+	data = (t_data *)ptr;
+	while (1)
 	{
-		pthread_mutex_lock(&philo->data->lock);
-		if (philo->data->fininshed >= philo->data->philo_num)
-			philo->data->dead = 1;
-		pthread_mutex_unlock(&philo->data->lock);
+		if (check_death(data) < 0)
+			return (NULL);
+		pthread_mutex_lock(&data->lock);
+		if (data->fininshed >= data->philo_num)
+			data->dead = 1;
+		pthread_mutex_unlock(&data->lock);
 	}
-	// pthread_mutex_unlock(&philo->data->read);
 	return (NULL);
 }
 
 void	*master(void *ptr)
 {
-	t_philo	*philo;
+	t_data	*data;
+	int		i;
 
-	philo = (t_philo *)ptr;
-	// pthread_mutex_lock(&philo->data->read);
-	while (philo->data->dead == 0)
+	data = (t_data *)ptr;
+	i = -1;
+	pthread_mutex_lock(&data->start);
+	pthread_mutex_unlock(&data->start);
+	// ph_usleep(1000);
+	while (++i < data->philo_num)
 	{
-		pthread_mutex_lock(&philo->lock);
-		if (get_time() > philo->time_to_die && philo->eating == 0)
-			message(philo, "died");
-		if (philo->eat_cnt == philo->data->must_eat_num)
-		{
-			pthread_mutex_lock(&philo->data->lock);
-			philo->data->fininshed++;
-			philo->eat_cnt++;
-			pthread_mutex_unlock(&philo->data->lock);
-		}
-		pthread_mutex_unlock(&philo->lock);
+		ph_usleep(200);
+		if (check_death(data) < 0)
+			return (NULL);
+		pthread_mutex_lock(&data->philos[i].lock);
+		if (get_time() >= data->philos[i].time_to_die && data->philos[i].eating == 0)
+			message(&data->philos[i], "died");
+		pthread_mutex_unlock(&data->philos[i].lock);
 	}
-	// pthread_mutex_unlock(&philo->data->read);
 	return (NULL);
 }
 
@@ -46,40 +45,52 @@ void	*routine(void *ptr)
 	t_philo	*philo;
 	
 	philo = (t_philo *)ptr;
+	pthread_mutex_lock(&philo->data->start);
+	pthread_mutex_unlock(&philo->data->start);
+	// ph_usleep(100);
+	if ((philo->idx & 1) == 0)
+		ph_usleep(100);
 	philo->time_to_die = philo->data->death_time + get_time();
-	if (pthread_create(&philo->t, NULL, master, philo) < 0)
-		return ((void *)-1);
-	while (philo->data->dead == 0)
+	while (1)
 	{
+		if (check_death(philo->data) < 0)
+			return (NULL);
 		eat(philo);
 		message(philo, "is thinking");
+		if (philo->eat_cnt == philo->data->must_eat_num)
+		{
+			pthread_mutex_lock(&philo->data->lock);
+			philo->data->fininshed++;
+			pthread_mutex_unlock(&philo->data->lock);
+		}
 	}
-	// check pthread join error
-	pthread_join(philo->t, NULL);
 	return (NULL);
 }
 
-
 int	init_thread(t_data *data)
 {
-	pthread_t	monitor_t;
 	int	i;
 
 	data->start_time = get_time();
+	pthread_mutex_lock(&data->start);
+	i = -1;
 	if (data->must_eat_num > 0)
 	{
-		if (pthread_create(&monitor_t, NULL, monitor, &data->philos[0]) < 0)
+		if (pthread_create(&data->monitor_t, NULL, monitor, data) < 0)
 			return (-1);
 	}
-	i = -1;
 	while (++i < data->philo_num)
 	{
 		if (pthread_create(&data->tid[i], NULL, routine, &data->philos[i]) < 0)
 			return (-1);
 		ph_usleep(1);
 	}
+	pthread_create(&data->master_t, NULL, master, data);
+	pthread_mutex_unlock(&data->start);
 	i = -1;
 	while (++i < data->philo_num)
 		pthread_join(data->tid[i], NULL);
+	pthread_join(data->monitor_t, NULL);
+	pthread_join(data->master_t, NULL);
 	return (0);
 }
